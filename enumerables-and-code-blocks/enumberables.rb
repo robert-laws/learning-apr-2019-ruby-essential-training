f8ca32d7906cd997567882c2160d7cbd4739e282
Robert Laws <roblaws@me.com>
2019-04-16 14:52:59 +0300

    begin enumerables section

---
+++ enumerables-and-code-blocks/enumberables.rb
@@ -0,0 +1,7 @@
+# Enumerables
+
+# countable items - arrays, ranges, hashes, strings (chars vs bytes)
+
+# included module -> enumerables
+
+# important enumerable methods - count, first, last, each, each_with_index, include?, max & min (if comparable)
