fb3907361a8ab267e5d4f34eb54a36b80f264b66
Robert Laws <roblaws@me.com>
2019-04-16 11:33:46 +0300

    add basic validation for number within range

--- ruby-scripting/guessing-game.rb
+++ ruby-scripting/guessing-game.rb
@@ -9,14 +9,19 @@ puts "|------------------|"
 
 while count <= 3 do
   print "Select a number between 1 and 10: "
-  user_guess = gets.chomp
-  if user_guess.to_i == random_number
-    puts "You guessed the number: #{random_number}!"
-    break
-  elsif(count != 3)
-    puts "The number is not correct, try again..."
+  user_guess = gets.chomp.to_i
+  if user_guess.between?(1, 10)
+    if user_guess == random_number
+      puts "You guessed the number: #{random_number}!"
+      break
+    elsif(count != 3)
+      puts "The number is not correct, try again..."
+    else
+      puts "Sorry, you used all your guesses. The number was #{random_number}."
+    end
   else
-    puts "Sorry, you used all your guesses. The number was #{random_number}."
+    puts "Please select a number between 1 and 10"
+    next
   end
   count += 1
 end
